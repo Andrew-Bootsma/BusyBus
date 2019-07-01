130c527914655449b6d7069ecfd85774791da980
Andrew Bootsma <a_bootsma@outlook.com>
2019-07-01 18:20:40 -0400

    finish app

---
+++ app.js
@@ -0,0 +1,7 @@
+document.getElementById('reroute-header').addEventListener('click', function() {
+    document.getElementById('reroute-list').classList.toggle('display-none');
+})
+
+document.getElementById('out-of-order-header').addEventListener('click', function() {
+    document.getElementById('out-of-order-list').classList.toggle('display-none');
+})
